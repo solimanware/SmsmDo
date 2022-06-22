0682374b1bb8bdbe9c07bc47bb0e49f72961d2cf
Osama Soliman <prof.o.soliman@gmail.com>
2022-06-22 10:49:19 +0200

    Initial Commit

---
+++ Shared/smsmdoApp.swift
@@ -0,0 +1,17 @@
+//
+//  smsmdoApp.swift
+//  Shared
+//
+//  Created by Osama Soliman on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct smsmdoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
